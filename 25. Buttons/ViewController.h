dfe3e40922581fe409077dbf689be407647d82a6
Nikita Semakov <n.semakov@yahoo.com>
2015-08-04 21:48:43 +0300

    Initial Commit

---
+++ 25. Buttons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  25. Buttons
+//
+//  Created by Admin on 04.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
